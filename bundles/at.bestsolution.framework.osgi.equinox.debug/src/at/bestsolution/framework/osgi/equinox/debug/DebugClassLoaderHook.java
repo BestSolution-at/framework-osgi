c1f094d56d2a8699442e1dde7872c6bfad1931f8
Tom Schindl <tom.schindl@bestsolution.at>
2014-11-12 17:27:11 +0100

    logging to file, create module classloader for loggin

--- bundles/at.bestsolution.framework.osgi.equinox.debug/src/at/bestsolution/framework/osgi/equinox/debug/DebugClassLoaderHook.java
+++ bundles/at.bestsolution.framework.osgi.equinox.debug/src/at/bestsolution/framework/osgi/equinox/debug/DebugClassLoaderHook.java
@@ -2,16 +2,25 @@ package at.bestsolution.framework.osgi.equinox.debug;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 import java.util.regex.Pattern;
 
+import org.eclipse.osgi.internal.debug.Debug;
+import org.eclipse.osgi.internal.framework.EquinoxConfiguration;
 import org.eclipse.osgi.internal.hookregistry.ClassLoaderHook;
+import org.eclipse.osgi.internal.loader.BundleLoader;
 import org.eclipse.osgi.internal.loader.ModuleClassLoader;
+import org.eclipse.osgi.internal.loader.classpath.ClasspathManager;
+import org.eclipse.osgi.storage.BundleInfo.Generation;
 
 public class DebugClassLoaderHook extends ClassLoaderHook {
 	private Properties properties;
 	private Pattern prefindClassPattern;
+	private File outFile;
 
 	public DebugClassLoaderHook() {
 		properties = new Properties();
@@ -20,12 +29,35 @@ public class DebugClassLoaderHook extends ClassLoaderHook {
 			in = new FileInputStream(new File(System.getProperty("classloader.debug.config")));
 			properties.load(in);
 
-			if( properties.getProperty("pre.find.classpattern") != null ) {
-				System.err.println("Compiling: " + properties.getProperty("pre.find.classpattern"));
+			if( properties.getProperty("logfile") != null ) {
+				outFile = new File(properties.getProperty("logfile"));
+			}
+
+			if( properties.getProperty("classpattern") != null ) {
+				println("Compiling: " + properties.getProperty("pre.find.classpattern"));
 				prefindClassPattern = Pattern.compile(properties.getProperty("pre.find.classpattern"));
 			}
 		} catch( Throwable e ) {
 			e.printStackTrace();
+		}
+	}
+
+	public void println(String message) {
+		if( outFile == null ) {
+			System.out.println(message);
+		} else {
+			try {
+				FileWriter w = new FileWriter(outFile,true);
+				w.write(message + "\n");
+				w.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 		}
 	}
 
@@ -38,23 +70,78 @@ public class DebugClassLoaderHook extends ClassLoaderHook {
 	}
 
 	@Override
-	public Class<?> preFindClass(String name, ModuleClassLoader classLoader)
-			throws ClassNotFoundException {
-		if( ! trackFind(name) ) {
-			return super.preFindClass(name, classLoader);
+	public ModuleClassLoader createClassLoader(ClassLoader parent,
+			EquinoxConfiguration configuration, BundleLoader delegate,
+			Generation generation) {
+		return new DelegatingModuleClassloader(parent, configuration, delegate, generation);
+	}
+
+	class DelegatingModuleClassloader extends ModuleClassLoader {
+		private final EquinoxConfiguration configuration;
+		private final BundleLoader delegate;
+		private final Generation generation;
+
+		private ClasspathManager classpathManager;
+
+		public DelegatingModuleClassloader(ClassLoader parent, EquinoxConfiguration configuration,
+				BundleLoader delegate, Generation generation) {
+			super(parent);
+			this.configuration = configuration;
+			this.delegate = delegate;
+			this.generation = generation;
+			this.classpathManager = new ClasspathManager(generation, this) {
+				@Override
+				public Class<?> findLocalClass(String classname)
+						throws ClassNotFoundException {
+
+					if( ! trackFind(classname) ) {
+						return super.findLocalClass(classname);
+					}
+
+					println("Loading local '"+classname+"'");
+					try {
+						Class<?> c = super.findLocalClass(classname);
+						if( c != null ) {
+							println("Loaded class by '"+c.getClassLoader()+"'");
+						}
+						return c;
+					} catch( RuntimeException e ) {
+						throw e;
+					}
+
+				}
+			};
 		}
 
-		System.err.println("PRE: Searching class '"+name+"' in " + classLoader);
-		try {
-			Class<?> preFindClass = super.preFindClass(name, classLoader);
-			if( preFindClass != null ) {
-				System.err.println("PRE: Found class and loaded with '"+preFindClass.getClassLoader()+"'");
-			}
-			return preFindClass;
-		} catch(RuntimeException e) {
-			System.err.println("PRE: Failure while loading class '"+name+"'");
-			e.printStackTrace();
-			throw e;
+		@Override
+		protected Generation getGeneration() {
+			return generation;
 		}
+
+		@Override
+		protected Debug getDebug() {
+			return this.configuration.getDebug();
+		}
+
+		@Override
+		public ClasspathManager getClasspathManager() {
+			return classpathManager;
+		}
+
+		@Override
+		protected EquinoxConfiguration getConfiguration() {
+			return this.configuration;
+		}
+
+		@Override
+		public BundleLoader getBundleLoader() {
+			return delegate;
+		}
+
+		@Override
+		public boolean isRegisteredAsParallel() {
+			return false;
+		}
+
 	}
 }
