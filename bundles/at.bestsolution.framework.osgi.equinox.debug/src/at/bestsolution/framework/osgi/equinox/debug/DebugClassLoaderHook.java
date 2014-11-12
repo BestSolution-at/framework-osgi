35f9414fdc58b9c5d4b025941776933c92530892
Tom Schindl <tom.schindl@bestsolution.at>
2014-11-12 15:34:35 +0100

    added config option

--- bundles/at.bestsolution.framework.osgi.equinox.debug/src/at/bestsolution/framework/osgi/equinox/debug/DebugClassLoaderHook.java
+++ bundles/at.bestsolution.framework.osgi.equinox.debug/src/at/bestsolution/framework/osgi/equinox/debug/DebugClassLoaderHook.java
@@ -1,12 +1,49 @@
 package at.bestsolution.framework.osgi.equinox.debug;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
 import org.eclipse.osgi.internal.hookregistry.ClassLoaderHook;
 import org.eclipse.osgi.internal.loader.ModuleClassLoader;
 
 public class DebugClassLoaderHook extends ClassLoaderHook {
+	private Properties properties;
+	private Pattern prefindClassPattern;
+
+	public DebugClassLoaderHook() {
+		properties = new Properties();
+		InputStream in = null;
+		try {
+			in = new FileInputStream(new File(System.getProperty("classloader.debug.config")));
+			properties.load(in);
+
+			if( properties.getProperty("pre.find.classpattern") != null ) {
+				System.err.println("Compiling: " + properties.getProperty("pre.find.classpattern"));
+				prefindClassPattern = Pattern.compile(properties.getProperty("pre.find.classpattern"));
+			}
+		} catch( Throwable e ) {
+			e.printStackTrace();
+		}
+	}
+
+	private boolean trackFind(String name) {
+		if( prefindClassPattern == null ) {
+			return false;
+		}
+
+		return prefindClassPattern.matcher(name).find();
+	}
+
 	@Override
 	public Class<?> preFindClass(String name, ModuleClassLoader classLoader)
 			throws ClassNotFoundException {
+		if( ! trackFind(name) ) {
+			return super.preFindClass(name, classLoader);
+		}
+
 		System.err.println("PRE: Searching class '"+name+"' in " + classLoader);
 		try {
 			Class<?> preFindClass = super.preFindClass(name, classLoader);
@@ -20,77 +57,4 @@ public class DebugClassLoaderHook extends ClassLoaderHook {
 			throw e;
 		}
 	}
-
-	@Override
-	public Class<?> postFindClass(String name, ModuleClassLoader classLoader)
-			throws ClassNotFoundException {
-		System.err.println("POST: Searching class '"+name+"' in " + classLoader);
-		try {
-			Class<?> postFindClass = super.postFindClass(name, classLoader);
-			if( postFindClass != null ) {
-				System.err.println("POST: Found class and loaded with '"+postFindClass.getClassLoader()+"'");
-			}
-			return postFindClass;
-		} catch( RuntimeException e ) {
-			System.err.println("POST: Failure while loading class '"+name+"'");
-			e.printStackTrace();
-			throw e;
-		}
-
-	}
-
-
-//	@Override
-//	public ModuleClassLoader createClassLoader(ClassLoader parent,
-//			EquinoxConfiguration configuration, BundleLoader delegate,
-//			Generation generation) {
-//		ModuleClassLoader originalLoader = super.createClassLoader(parent, configuration, delegate, generation);
-//
-//		return new DebuggingClassloader(parent);
-//	}
-
-//	static class DebuggingClassloader extends ModuleClassLoader {
-//		private ModuleClassLoader originalLoader;
-//
-//		public DebuggingClassloader(ClassLoader parent) {
-//			super(parent);
-//		}
-//
-//		@Override
-//		protected Generation getGeneration() {
-//			// TODO Auto-generated method stub
-//			return null;
-//		}
-//
-//		@Override
-//		protected Debug getDebug() {
-//			// TODO Auto-generated method stub
-//			return null;
-//		}
-//
-//		@Override
-//		public ClasspathManager getClasspathManager() {
-//			// TODO Auto-generated method stub
-//			return null;
-//		}
-//
-//		@Override
-//		protected EquinoxConfiguration getConfiguration() {
-//			// TODO Auto-generated method stub
-//			return null;
-//		}
-//
-//		@Override
-//		public BundleLoader getBundleLoader() {
-//			// TODO Auto-generated method stub
-//			return null;
-//		}
-//
-//		@Override
-//		public boolean isRegisteredAsParallel() {
-//			// TODO Auto-generated method stub
-//			return false;
-//		}
-//
-//	}
 }
--- bundles/at.bestsolution.framework.osgi.equinox.debug/src/at/bestsolution/framework/osgi/equinox/debug/DebugHookConfigurator.java
+++ bundles/at.bestsolution.framework.osgi.equinox.debug/src/at/bestsolution/framework/osgi/equinox/debug/DebugHookConfigurator.java
@@ -4,6 +4,9 @@ import org.eclipse.osgi.internal.hookregistry.HookConfigurator;
 import org.eclipse.osgi.internal.hookregistry.HookRegistry;
 
 public class DebugHookConfigurator implements HookConfigurator {
+	public DebugHookConfigurator() {
+		System.err.println("Debug hook is active");
+	}
 
 	@Override
 	public void addHooks(HookRegistry hookRegistry) {
